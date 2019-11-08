2480c22cb22174da27f4f51f4386fe2eaa53008e
bardiademon <bardiademon@gmail.com>
2019-11-08 22:18:23 +0330

    First commit

---
+++ src/main/java/com/bardiademon/CyrusMessenger/CyrusMessengerApplication.java
@@ -0,0 +1,15 @@
+package com.bardiademon.CyrusMessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyrusMessengerApplication
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (CyrusMessengerApplication.class , args);
+    }
+
+}
---
+++ src/test/java/com/bardiademon/CyrusMessenger/CyrusMessengerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bardiademon.CyrusMessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyrusMessengerApplicationTests
+{
+
+    @Test
+    void contextLoads ()
+    {
+    }
+
+}
