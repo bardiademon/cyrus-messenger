38a865395bf5de11339a5d2e2b98cc79cc2ed642
bardiademon <bardiademon@gmail.com>
2020-01-19 00:42:15 +0330

    Remove email in register , Because I need approval, I built a separate class

--- src/main/java/com/bardiademon/CyrusMessenger/Controller/Rest/RestRegister/RegisterRequest.java
+++ src/main/java/com/bardiademon/CyrusMessenger/Controller/Rest/RestRegister/RegisterRequest.java
@@ -5,16 +5,15 @@ public final class RegisterRequest
 
     private String phone;
 
-    public final String username, email, region;
+    public final String username, region;
     public final String name, family;
 
     public final String password;
 
-    public RegisterRequest (String phone , String username , String email , String region , String name , String family , String password)
+    public RegisterRequest (String phone , String username , String region , String name , String family , String password)
     {
         this.phone = phone;
         this.username = username;
-        this.email = email;
         this.region = region;
         this.name = name;
         this.family = family;
